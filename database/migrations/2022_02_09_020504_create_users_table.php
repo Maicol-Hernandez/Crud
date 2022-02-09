1decda0e6a0d07b8f02066c9807145b39ee0a54e
MaicolHernandez <maicolhernandez420@gmail.com>
2022-02-09 12:32:44 -0500

    Se agrega la relacion y el tipo del usuario

---
+++ database/migrations/2022_02_09_020504_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->engine = "InnoDB";
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->bigInteger('identificacion')->unique();
+            $table->string('photo');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->bigInteger('tipo')->unsigned(); // 1 = agente, 2 = supervisor, 3 = administrador
+            $table->rememberToken();
+            $table->foreign("tipo")->references("id")->on("rol_usuarios")->onDelete("cascade");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/migrations/2022_02_09_063410_usuarios.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Usuarios extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('usuarios', function (Blueprint $table) {
+            
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8mb4';
+    $table->collation = 'utf8mb4_unicode_ci';
+
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->bigInteger('identificacion')->unsigned()->unique();
+            $table->string('photo');
+            $table->string('password')->nullable();
+            $table->bigInteger('rol_id')->unsigned()->unique();
+            // $table->foreign("rol_id")->references("id")->on("rol_usuarios")->onDelete("cascade");
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('usuarios');
+
+    }
+}
